ef7423f53b5bc828d3f89d180c9b7424a49e62d1
hilar <hiloriospoy@gmail.com>
2024-03-11 17:48:41 +0100

    4 -> Método trimestreLluvioso

--- src/es/iesmz/tests/DiasLluvia.java
+++ src/es/iesmz/tests/DiasLluvia.java
@@ -34,4 +34,58 @@ public class DiasLluvia {
         }
         return contador;
     }
+
+    public int trimestreLluvioso() {
+        int contador = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 31; j++) {
+                if (this.calendario[j][i]) {
+                    contador++;
+                }
+            }
+            t1 = contador;
+        }
+        contador = 0;
+        for (int i = 3; i < 6; i++) {
+            for (int j = 0; j < 31; j++) {
+                if (this.calendario[j][i]) {
+                    contador++;
+                }
+            }
+            t2 = contador;
+        }
+        contador = 0;
+        for (int i = 6; i < 9; i++) {
+            for (int j = 0; j < 31; j++) {
+                if (this.calendario[j][i]) {
+                    contador++;
+                }
+            }
+            t3 = contador;
+        }
+        contador = 0;
+        for (int i = 9; i < 12; i++) {
+            for (int j = 0; j < 31; j++) {
+                if (this.calendario[j][i]) {
+                    contador++;
+                }
+            }
+            t4 = contador;
+        }
+
+        int mayorTrimestre = t1;
+        int[] valores = {t1, t2, t3, t4};
+
+        for (int i = 1; i < valores.length; i++) {
+            if (valores[i] > mayorTrimestre) {
+                mayorTrimestre = valores[i];
+            } else {
+                return mayorTrimestre;
+            }
+        }
+        return mayorTrimestre;
+    }
 }
+
+
